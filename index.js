505bbc15b8cab3b463cc50aa8e4e5345b76564b2
nandana <nandanarayaroth7@gmail.com>
2025-09-12 13:38:19 +0530

    thirdcommit

--- index.js
+++ index.js
@@ -655,19 +655,361 @@
 
 // calcAverage([2,3,6])
 
-const measureKelvin = function() {
-    const measurement = {
-        type: 'temp',
-        unit: 'celsius',
-        value : Number(prompt('Degree celsius'))
+// const measureKelvin = function() {
+//     const measurement = {
+//         type: 'temp',
+//         unit: 'celsius',
+//         value : Number(prompt('Degree celsius'))
+//     }
+
+//     console.log(measurement.value)
+//     console.warn(measurement.value)
+//     console.error(measurement.value)
+
+//     const kelvin = measureKelvin.value + 273
+//     return kelvin
+
+// }
+// console.log(measureKelvin())
+
+// 'use strct'
+
+// function calAge(birthYear) {
+//     const age = 2037 - birthYear
+//     function printAge(){
+//         const output = `${firstName} You are ${age}, born in ${birthYear}`
+//         console.assert(output)
+
+//         if(birthYear >= 1981 && birthYear <= 1996) {
+//             var millenial = true
+//             const str = `Oh, and you're a millenial, ${firstName}`
+//             console.log(str)
+
+        
+//         }
+//         // console.log(str)
+//         console.log(millenial)
+//     }
+//     printAge()
+//     return age
+// }
+
+// const firstName = 'Jonas'
+// calAge(1991)
+// console.log(age)
+// printAge()
+
+// console.log(me)
+// console.log(job)
+// console.log(year)
+
+
+// var me = 'Onemanshow'
+// let job = 'teacher'
+// const year = 1991
+
+// console.log(addDecl(2,3))
+// // console.log(addExpr(2,3))
+// console.log(addArrow)
+// // console.log(addArrow(2,3))
+
+// function addDecl(a,b) {
+//     return a+b
+// }
+
+// const addExpr = function (a,b) {
+//     return a+b
+// }
+// const addArrow = (a,b) => a+b
+
+// if(!numProducts) deleteShoppingCart()
+
+// var numProducts = 10
+
+// function deleteShoppingCart() {
+//     console.log('All product deleted')
+// }
+
+// this keyword
+// console.log(this)
+
+// const calAge = function (birthYear) {
+//     console.log(2037 - birthYear)
+//     // console.log(this)
+// }
+// calAge(1991)
+
+// const calAgeArrow = birthYear => {
+//     console.log(2037 - birthYear)
+//     // console.log(this)
+// }
+// calAgeArrow(1980)
+
+// const jonas = {
+//     year: 1991,
+//     calAge: function(){
+//         // console.log(this)
+//         console.log(2037 - this.year)
+//     }
+// }
+// jonas.calAge()
+
+// const matilda = {
+//     year: 2017
+// }
+// matilda.calAge = jonas.calAge
+
+// matilda.calAge()
+
+// const f = jonas.calAge
+// f()
+
+// var firstName = 'Matilda'
+
+// const jonas = {
+//     firstName: 'Jonas',
+//     year: 1991,
+//     calcAge : function(){
+//         console.log(this)
+//         console.log(2037 - this.year)
+
+//         const isMillenial = function(){
+//             console.log(this.year >= 1981 && this.year <= 1996 )
+//         }
+//         isMillenial()
+//     },
+//     greet : () => {
+//         console.log(this)
+//         console.log(`Hey ${this.firstName}`)
+//     },
+// }
+// jonas.greet()
+// jonas.calcAge()
+
+// const addExpr = function(a, b) {
+//     console.log(arguments)
+//     return a+b
+// }
+
+// addExpr(2, 5)
+// addExpr(2, 5, 8, 12)
+
+// var addArrow = (a, b) => {
+
+//     return a+b
+// }
+// // addArrow(2, 5, 8)
+// const jessica = {
+//     firstName :'Jessica',
+//     lastName : 'Williams',
+//     age: 27
+// };
+
+// const marriedJessica = jessica
+// marriedJessica.lastName ='Davis'
+
+// console.log('Before', jessica)
+// console.log('After', marriedJessica)
+
+// jessica.age = 30
+
+
+// 'use strict'
+
+// const restaurant = {
+//     name: 'classico Italiano',
+//     location : 'Via Angel Tavanti 23, Firenze, Italy',
+//     categories: ['Italian', 'Pizzeria', 'Vegetarian, Organic'],
+//     starteMenu : ['Focaccia', 'Brushetta', 'Garlic', 'Bread', 'Caprese Salad'],
+//     mainMenu : ['Pizza', 'Pasta', 'Risotto'],
+//     order: function(starterIndex, mainIndex) {
+//         return(this.starteMenu[starterIndex], this.mainMenu(mainIndex))
+//     } 
+// }
+
+// const arr = [2, 3, 4]
+// const a = arr[0]
+// const b = arr[1]
+// const c = arr[2]
+
+// const [x, y, z] = arr
+// console.log(x, y, z)
+// console.log(arr)
+
+// let [main, secondary] = restaurant.categories
+// console.log(main, secondary)
+
+// // const temp = main
+// // main = secondary
+// // secondary = temp
+// // console.log(main, secondary)
+
+// [main, secondary] = [secondary, main]
+// console.log(main, secondary)
+
+// const [stater , mainCourse] = restaurant.order(2, 0)
+// console.log(stater, mainCourse)
+
+// const nested = [2, 4, [5, 6]]
+// // const [i, j] = nested
+// // console.log(i, j)
+// const [i, [j,k]] = nested
+// console.log(i, j, k)
+
+
+// const [p = 1,q = 1,r = 1] = [8,9]
+// console.log(p,q,r)
+// 
+
+// // destructring object
+
+const restaurant = {
+    name: 'Classico Italiano',
+    location : 'Via Angelo Tavanti 23, Firenze, Italy',
+    categories : ['Italian', 'Pizzeria', 'Vegetaian', 'Organic'],
+    startMenu : ['Focaccia', 'Brushetta', 'Garlic Bread', 'Caprese Salad'],
+    mainMenu: ['Pizza', 'Pasta', 'Risoto'],
+    openingHours: {
+        thu : {
+            open: 12,
+            close: 22
+        },
+        fri: {
+            open: 12,
+            close: 23
+        },
+        sat: {
+            open: 0,
+            close: 24
+        }
+    },
+    order: function (starterIndex, mainIndex) {
+        return [this.startMenu[starterIndex], this.mainMenu[mainIndex]]
+    },
+
+    orderDelivery: function({starterIndex, mainIndex, time, address}) {
+        console.log(`Order recieved! ${this.startMenu [starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`)
+    },
+    orderPaste: function(ing1, ing2, ing3){
+        console.log(`Here is your declicious pasta with ${ing1}, ${ing2}, ${ing3}`)
+    },
+    orderPizza: function(mainIngredient, ...otherIngrediens) {
+        console.log(mainIngredient)
+        console.log(otherIngrediens)
     }
-
-    console.log(measurement.value)
-    console.warn(measurement.value)
-    console.error(measurement.value)
-
-    const kelvin = measureKelvin.value + 273
-    return kelvin
-
 }
-console.log(measureKelvin())
+
+restaurant.orderDelivery({
+    time: '22:30',
+    address: 'Via del Sole, 21',
+    mainIndex: 2,
+    starterIndex: 2
+})
+
+const guests = restaurant.numGuests || 10
+console.log(guests)
+
+const guestCorrect = restaurant.numGuests ?? 10 ;
+console.log(guestCorrect)
+
+// console.log(3 || 'Jonas')
+// console.log('' || 'Jonas')
+// console.log(true || 0)
+// console.log(undefined || null)
+// console.log(undefined || 0 || '' || 'Hello' || 23 || null)
+
+// restaurant.numGuests =  0
+// const guests1 = restaurant.numGuests ? restaurant.
+// numGuests : 10
+// console.log(guests1)
+
+// const guests2 = restaurant.numGuests || 10
+// console.log(guests2)
+
+// console.log('___AND___')
+// console.log(0 && 'Jonas')
+// console.log(7 && 'Jonas')
+// console.log('Hello' && 23 && null && 'jonas')
+
+// if(restaurant.orderPizza) {
+//     restaurant.orderPizza('mushroom', 'spinach')
+// }
+
+// restaurant.orderPizza && restaurant.orderPizza('mushroom', 'spinach')
+
+
+
+// const [Pizza, , ,risotto, ...otherFood] = [
+//     ...restaurant.mainMenu,
+//     ...restaurant.startMenu
+// ]
+// console.log(Pizza, risotto, otherFood)
+
+// const { sat, ...weekdays } = restaurant.openingHours
+// console.log(weekdays)
+
+
+// const add = function(...numbers) {
+//     let sum = 0
+
+//     for(let i = 0; i<numbers.length; i++){
+//         sum += numbers[i]
+//     }
+//     console.log(sum)
+// }
+
+// add(2,3)
+// add(5, 3, 7, 2)
+// add(8, 2, 5, 3, 2, 1, 4)
+
+// const x = [23, 5, 7]
+// add(...x)
+
+// restaurant.orderPizza('mushrooms', 'onion', 'olives', 'spinach')
+// restaurant.orderPizza('mushrooms')
+
+
+// const { name, openingHours, categories} = restaurant 
+// console.log( name, openingHours, categories)
+
+// const {name: restaurantName, openingHours: hours, categories: tags} = restaurant
+// console.log(restaurantName, hours, tags)
+
+// const { menu = [], startMenu: staters = []} = restaurant
+// console.log(menu, staters)
+
+// let a = 111
+// let b = 999
+// const obj = { a: 23, b: 7, c: 14 }
+
+// // ({a, b} = obj)
+// // console.log(a, b)
+
+// // Nested objects
+// const {fri: { open: o, close: c}} = openingHours
+// console.log(o, c)
+
+// // Spreda Operator
+// const arr = [7, 8, 9]
+// const badNewArr = [1, 2, arr[0], arr[1], arr[2]]
+// console.log(badNewArr)
+
+// const newArr = [1, 2, ...arr]
+// console.log(newArr)
+
+// console.log(...newArr)
+
+// const newMenu = [...restaurant.mainMenu, 'Gnocci']
+// console.log(newMenu)
+
+// const mainMenuCopy = [...restaurant.mainMenu]
+// const menuu = [...restaurant.startMenu, ...restaurant.mainMenu]
+// console.log(menuu)
+
+// const str = 'Jonas'
+// const letters = [...str, '', 's. ']
+// console.log(letters)
+// console.log(letters)
+// console.log(...str)
+
+// const ingredients = [prompt(`let's make pasta! Ingredient 1?`)]
--- note.js
+++ note.js
@@ -37,4 +37,41 @@
 
 // DOM 
 // Document Object Model: structured representation of html documents allowss javascript to access html elements and styles to manipulate them
+
+// javascript engine : a computer program that execute javascript code 
+// compliation enitere code is converted into machine code at once and written to a binary files that can be executed by a computer
+// interpretation : interpreter runs through the source code and execute it line by line 
+// just in time compilation entire code is converted into machine code at onec then excute immediatly
 // 
+// 
+
+// scoping and scope in javascipt 
+// scoping how our program's variables are organized and accessed "where do varibales live?" or "Where can we access a certain varibale and where not?"
+// lexical scoping:Scoping is contolled by placement of functionas and blocks in the code
+//
+// there are 3 different type of scopes 
+// global scope: 
+// *)outside of any fucntion or block , 
+// *)variable declated in global scope are accessible everywhere
+
+// function scope:
+// *) Variables are accessible only inside function, NOT outside
+// *) also called loacl scope
+
+// Block scope:
+// *)variables are accessible only inside block (block scoped)
+// *)HOWEVER this only applies to let and const variables!
+// *)functions are also block scoped (only in strict mode)
+
+
+// Hoisting: make some types of varible accessible/usable in the code before they are actually declared variable lifted to the top of their scope
+// Before execution code is scanned for variable declaration and each variable a new property is called in the variable enviornment object
+
+// this keyword: special variable that is created for every exectution context (every function ) take the value of (point to ) the owner of the function in which the this keyword is used
+// this is not static it depends on how the function is called, and its value is only assigned when the function is actually called
+
+// primitive datatypes :-> Number ,string, Boolean, undefined, Null, symbol, BigInt
+// objects :-> object literals, Arrays, FUnctions, many more...
+
+// memory lifecycle
+// are stored global execution context which
